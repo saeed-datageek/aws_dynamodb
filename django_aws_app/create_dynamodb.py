da3b58d4b04eee0f6f494ae8a8e84f4a28c86500
saeed-datageek <saeed.azad86p1@gmail.com>
2023-10-04 17:20:28 +0330

    add dynamodb table with boto3

---
+++ django_aws_app/create_dynamodb.py
@@ -0,0 +1,27 @@
+import boto3
+def create_dynamodb_table():
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.create_table(
+        TableName='s.azad_device_table',
+        KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'S'
+            },
+
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        }
+    )
+
+    table.wait_until_exists()
+
+    return table
